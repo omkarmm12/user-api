8da485532f434835cebc7353ae902eb0f405f73b
omkar <degavathomkar934@gmail.com>
2024-12-07 11:03:21 +0530

    remove docker

--- src/main/java/com/omkar/user_api/UserApiApplication.java
+++ src/main/java/com/omkar/user_api/UserApiApplication.java
@@ -2,6 +2,9 @@ package com.omkar.user_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserApiApplication {
@@ -10,4 +13,16 @@ public class UserApiApplication {
 		SpringApplication.run(UserApiApplication.class, args);
 	}
 
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> customizer() {
+		return factory -> {
+			// Dynamically set the port from the PORT environment variable
+			String port = System.getenv("PORT");
+			if (port != null) {
+				factory.setPort(Integer.parseInt(port));
+			} else {
+				factory.setPort(8080); // Default to port 8080 if PORT is not set
+			}
+		};
+	}
 }
